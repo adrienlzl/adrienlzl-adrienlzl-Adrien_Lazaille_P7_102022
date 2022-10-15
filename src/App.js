1e6757ccbc69c0a416cb4c0bb58f5c52e03f3af1
adrienlzl <lazailleadrien@gmail.com>
2022-10-15 16:57:43 +0200

    Premier commit du Kasa: Creation de l'app React Créations des pages (Home, About, Error et Card), mise en place de l'HTML et du css. Création des Routes avec React-Router-Dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,} from 'react-router-dom'
+import './css/App.css'
+import RouteLayout from "./pages/RouteLayout";
+//Pages du site
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Card from "./pages/Card";
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<RouteLayout />}>
+          <Route index element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/card' element={<Card />}/>
+        </Route>
+    ));
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+  return <RouterProvider router={router}/>
 }
 
 export default App;
+
